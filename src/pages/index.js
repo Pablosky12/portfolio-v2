c3db10cce993c3bca15ddc80bd9d2c197d7719a2
Stiv <pabloml@kzsoftworks.com>
2021-04-16 22:44:11 -0300

    Add title adn description

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,34 +9,49 @@ const IndexPage = () => {
     <Layout>
       <Main>
         <Header>
-          I'm a<em> Full stack developer </em> with lots of passion and
-          dolorsitamet abasdbasd{" "}
+          I'm a<em> Web developer </em> who loves to build <em>quality</em>{" "}
+          products with cool tech.
         </Header>
         <PresentationCard>
           <Headshot src={headBw} alt="Pablo Marcano headshot" />
           <div>
             <p className="name">Pablo Marcano</p>
-            <p className="desc">
-              Montevideo, Uruguay + Remote
-            </p>
-            <p className="desc">
-              Available for Contract Work
-            </p>
+            <p className="desc">Montevideo, Uruguay + Remote</p>
+            <p className="desc">Available for Contract Work</p>
           </div>
         </PresentationCard>
       </Main>
+      <Divider>
+        <a href="#work">See work ▼ </a>
+      </Divider>
     </Layout>
   );
 };
 
 const Header = styled.h2`
   font-size: 3em;
+  line-height: 1.3em;
   color: ${({ theme }) => theme.text};
+  max-width: min(80%, 1080px);
   em {
     background-color: ${({ theme }) => theme.contrast1};
     color: ${({ theme }) => theme.background};
     font-style: normal;
     font-weight: bold;
+  }
+  @media (min-height: 748px) {
+    padding: 30% 0;
+  }
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    color: ${({ theme }) => theme.text};
+    text-decoration: underline;
   }
 `;
 
@@ -49,7 +64,7 @@ const Headshot = styled.img`
   object-position: 30% 50%;
 `;
 const PresentationCard = styled.div`
-  display: flex;  
+  display: flex;
   align-items: center;
   font-size: 0.8em;
   p {
